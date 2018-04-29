0f5ecb15699aaf8746f635b478398570c4b59390
tehelbaru <36852188+tehelbaru@users.noreply.github.com>
2018-04-29 17:40:39 +0700

    Create mswsd.cpp

---
+++ mswsd.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+pair< vector<vector<int> >, vector<vector<bool> > > init(int besar)
+{
+    vector<vector<int> > layerBack;
+    vector<vector<bool> > layerFront;
+
+    for(int i=0;i<besar;i++){
+        vector<int> BackTemp;
+        vector<bool> FrontTemp;
+        for(int j=0;j<besar;j++){
+            BackTemp.push_back(0);
+            FrontTemp.push_back(true);
+        }
+        layerBack.push_back(BackTemp);
+        layerFront.push_back(FrontTemp);
+
+        BackTemp.clear();
+        FrontTemp.clear();
+    }
+
+    return make_pair(layerBack,layerFront);
+}
+
+void Display(vector<vector<int> > temp)
+{
+    for(int i=0;i<temp.size();i++){
+        for(int j=0;j<temp[i].size();j++){
+            if(temp[i][j]==-1) cout<<"B ";
+            else if(temp[i][j]==0) cout<<"  ";
+            else cout << temp[i][j] << " ";
+        }
+        cout<<endl;
+    }
+}
+
+void makebomb(int nbomb, vector<vector<int> > &layer)
+{
+    srand(time(NULL));
+    int besar=layer.size();
+
+    for(int i=0;i<nbomb;i++){
+        int x=rand() % besar;
+        int y=rand() % besar;
+
+        if(layer[x][y]!=-1) layer[x][y]=-1;
+        else i--;
+    }
+}
+
+void makenum(vector<vector<int> > &layer)
+{
+    for(int i=0;i<layer.size();i++){
+        for(int j=0;j<layer[i].size();j++){
+            if(layer[i][j]==-1){
+                if(i-1>=0){
+                    if(j-1>=0)layer[i-1][j-1]++;
+                    layer[i-1][j]++;
+                    if(j+1<layer[i].size()) layer[i-1][j+1]++;
+                }
+                if(j-1>=0)layer[i][j-1]++;
+                if(j+1<layer[i].size())layer[i][j+1]++;
+                if(i+1<layer.size()){
+                    if(j-1>=0)layer[i+1][j-1]++;
+                    layer[i+1][j]++;
+                    if(j+1<layer[i].size())layer[i+1][j+1]++;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    int besar=16;
+
+    pair< vector<vector<int> >, vector<vector<bool> > > res=init(besar);
+
+    vector<vector<int> > layerBack=res.first;
+    vector<vector<bool> > layerFront=res.second;
+
+    makebomb(256,layerBack);
+    makenum(layerBack);
+    Display(layerBack);
+}
